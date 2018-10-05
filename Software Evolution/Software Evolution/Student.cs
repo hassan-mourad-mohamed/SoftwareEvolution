9dbefc7bf01a4722445af764414ffc33cb5f80f1
hassan-mourad-mohamed <h.morad@fci-cu.edu.eg>
2018-10-05 20:59:31 +0200

    added file "student.cs"

---
+++ Software Evolution/Software Evolution/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Evolution
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
