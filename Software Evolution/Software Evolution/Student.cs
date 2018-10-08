116dc3e860733aed31f00b60fd76f63f9cb0fc9f
hassan-wolverine <hassanwolverin@gmail.com>
2018-10-08 13:58:05 +0200

    added "phone number"

--- Software Evolution/Software Evolution/Student.cs
+++ Software Evolution/Software Evolution/Student.cs
@@ -12,5 +12,7 @@ namespace Software_Evolution
         public double GPA { get; set; }
         public LevelEnum level { get; set; }
         public string Address { get; set; }
+
+        public string MobilePhone { get; set; }
     }
 }
